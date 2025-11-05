a2eeb36c439ae37821e4dfdc49f1c0ce901b8ea4
Joel Josafat Hernandez Saucedo <jojohersa21@gmail.com>
2025-11-04 21:21:16 -0600

    Refactor main.py to enhance root endpoint response and improve environment-based server configuration

--- app/main.py
+++ app/main.py
@@ -1,12 +1,23 @@
+import os
 import uvicorn
 from fastapi import FastAPI
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = FastAPI(title="DevMetrics API", version="1.0.0")
 
 
 @app.get("/")
-async def root():
-    return {"ok": True}
+async def read_root():
+    return {"message": "Welcome to the DevMetrics API", "version": app.version, "ok": True}
 
 if __name__ == "__main__":
-    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
+    env = os.getenv("ENV", "development")
+
+    if env == "production":
+        port = int(os.getenv("PORT", 8000))
+        uvicorn.run("main:app", host="0.0.0.0", port=port)
+
+    else:
+        uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
