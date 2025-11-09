a9a691af294a7f1b81fe765a960c37284ea15217
Joel Josafat Hernández Saucedo <jojohersa21@gmail.com>
2025-11-09 09:45:34 -0600

    feat: implement project management routes with CRUD operations for authenticated users (close #9)

--- app/security.py
+++ app/security.py
@@ -71,3 +71,13 @@ def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(
 
     return user
         
+
+def get_current_active_user(current_user: models.User = Depends(get_current_user)):
+    """
+    Verifica que el usuario actual este activo
+    """
+
+    if not current_user.is_active:
+        raise HTTPException(status_code=400, detail="Inactive user")
+    
+    return current_user
